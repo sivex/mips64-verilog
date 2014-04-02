435e5670fb0cd7918b3de0a2a5655f29a50c1215
giropau1 <giropau1@umbc.edu>
2014-04-01 22:22:26 -0400

    Update data_MEM.v

--- data_MEM.v
+++ data_MEM.v
@@ -28,14 +28,14 @@ module data_MEM(addr, d_in, d_out, en, wr, clk);
 	
 	// Main RAM code
 	always@ (posedge clk) begin
-		if (en) begin							// When en is on, the RAM is active.
-			if (wr) begin						// When wr is on, the corresponding address 
+		if (en) begin					// When en is on, the RAM is active.
+			if (wr) begin				// When wr is on, the corresponding address 
 				data_RAM[addr] <= d_in;		// is written in the Data RAM and outputted;
 				d_out <= d_in;
 			end
 			else begin
 				d_out <= data_RAM[addr];	// Send out the data at the address given 
-		end										// if it is a read (not wr). 
+		end						// if it is a read (not wr). 
 	end
 		
 		
