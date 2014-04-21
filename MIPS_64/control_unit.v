f243244582e53d3af910f5e653ca26ab35686960
Kevin <kevin.w.sears@gmail.com>
2014-04-21 10:53:21 -0400

    control update
    Still need to determine the actual control signals but this is a start.
    I also couldn't find the diagram we looked at on Thursday which might be
    helpful

--- MIPS_64/control_unit.v
+++ MIPS_64/control_unit.v
@@ -17,4 +17,66 @@ module control_unit(clk, inst, zero, RegDst, RegWrite, ALUSrc, ALUOp,
 	output RegDst, RegWrite, ALUSrc, PCSrc, PCen, MemWrite, MemRead, MemToReg;
 	output [3:0] ALUOp;
 
+	// type of instruction
+	reg R,I,J; 
+
+	reg[4:0] sourceReg, sourceReg2, destReg, shift;
+	reg[5:0] funct;
+	reg[15:0] immediateData;
+	reg[25:0] jumpData;
+
+	// inst.	OPCODE	TYPE
+	// add 	100000	R
+	// addi	001000	I
+	// lw		100011	I
+	// sw		101011	I
+	// jmp	000010	J
+	// beq	000100	I
+	// bne	000101	I
+	//
+	// determine type of instruction
+	always @(posedge clk) begin
+		reg opCode = inst[31:26];
+		if(opCode == 100000) begin
+			R = 1;
+			I = 0;
+			J = 0;
+			end
+		else if((opCode == 100011)||(opCode == 101011)||(opCode == 000100)||(opCode == 000101)) begin  
+			R = 0;
+			I = 1;
+			J = 0;
+			end
+		else if((opCode == 000010)) begin
+			R = 0;
+			I = 0;
+			J = 1;
+			end
+		end
+	// parse parts of instruction
+	always @(posedge clk) begin
+		// for all reg opCode = inst[31:26];
+
+		// for Register-Type
+		if (R) begin
+			reg sourceReg = inst[25:21];
+			reg sourceReg2 = inst[20:16];
+			reg destReg = inst[15:11];
+			reg shift = inst[10:6];
+			reg funct = inst[5:0];
+			end
+
+		// for Immediate-Type
+		else if (I) begin 
+			reg sourceReg = inst[25:21];
+			reg sourceReg2 = inst[20:16];
+			reg immediateData = inst[15:0];
+			end
+		
+		// for J- Type
+		else if (J) begin
+			reg jumpData = inst[25:0];
+			end
+		end
+
 endmodule
