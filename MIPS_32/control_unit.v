2c89b866667f5c544d27f0b1d6bb49bf496a4e8f
aarodav1 <aarodav1@umbc.edu>
2014-05-01 16:13:12 -0400

    control update

--- MIPS_32/control_unit.v
+++ MIPS_32/control_unit.v
@@ -6,24 +6,17 @@
 // Module Name: control_unit
 // Description: Control unit for the 64 bit MIPS CPU
 ////////////////////////////////////////////////////////////////////////////////
-module control_unit(clk, inst, zero, RegDst, RegWrite, ALUSrc, ALUOp,
+module control_unit(clk, inst, zero, ALUSrc, ALUOp,
 							PCSrc, PCen, MemWrite, MemRead, MemToReg);
 
-	parameter SIZE = 32;
 
 	input clk;
-	input [(SIZE-1):0] inst;
-	input zero;
+	input [4:0] inst;
 	output RegDst, RegWrite, ALUSrc, PCSrc, PCen, MemWrite, MemRead, MemToReg;
-	output [3:0] ALUOp;
+	output [1:0] ALUOp;
 
 	// type of instruction
 	reg R,I,J; 
-
-	reg[4:0] sourceReg, sourceReg2, destReg, shift;
-	reg[5:0] funct;
-	reg[15:0] immediateData;
-	reg[25:0] jumpData;
 
 	// inst.	OPCODE	TYPE
 	// add 	100000	R
@@ -36,18 +29,17 @@ module control_unit(clk, inst, zero, RegDst, RegWrite, ALUSrc, ALUOp,
 	//
 	// determine type of instruction
 	always @(posedge clk) begin
-		reg opCode = inst[31:26];
-		if(opCode == 100000) begin
+		if(inst == 100000) begin
 			R = 1;
 			I = 0;
 			J = 0;
 			end
-		else if((opCode == 100011)||(opCode == 101011)||(opCode == 000100)||(opCode == 000101)) begin  
+		else if((inst == 100011)||(inst == 101011)||(inst == 000100)||(inst == 000101)) begin  
 			R = 0;
 			I = 1;
 			J = 0;
 			end
-		else if((opCode == 000010)) begin
+		else if((inst == 000010)) begin
 			R = 0;
 			I = 0;
 			J = 1;
@@ -55,27 +47,21 @@ module control_unit(clk, inst, zero, RegDst, RegWrite, ALUSrc, ALUOp,
 		end
 	// parse parts of instruction
 	always @(posedge clk) begin
-		// for all reg opCode = inst[31:26];
-
 		// for Register-Type
 		if (R) begin
-			reg sourceReg = inst[25:21];
-			reg sourceReg2 = inst[20:16];
-			reg destReg = inst[15:11];
-			reg shift = inst[10:6];
-			reg funct = inst[5:0];
-			end
-
+			RegDst = 1;
+			ALUSrc = 0;
+			MemtoReg = 1;
+			ALUOp = 2'b10;
+		end
 		// for Immediate-Type
 		else if (I) begin 
-			reg sourceReg = inst[25:21];
-			reg sourceReg2 = inst[20:16];
-			reg immediateData = inst[15:0];
+
 			end
 		
 		// for J- Type
 		else if (J) begin
-			reg jumpData = inst[25:0];
+
 			end
 		end
 
