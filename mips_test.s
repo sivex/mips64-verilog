c87e8bab034d8f7ac2db1e8dc9344e87d2fe691a
Griffin Yourick <gryphon92@gmail.com>
2014-05-01 17:15:41 -0400

    Fix 'compiled' code, update mips documentation

--- mips_test.s
+++ mips_test.s
@@ -1,30 +1,40 @@
 # Required Operations:
-# - add
-# - addi
-# - beq
-# - bne
-# - jmp
-# - lw
-# - sw
+# - add  _x_
+# - addi _x_
+# - beq  ___
+# - bne  ___
+# - jmp  ___
+# - lw   ___
+# - sw   _x_
+
+# 'Compiled' code is in `instructions.txt`
 
 # Code goes here
 .text
-  # put 1 in $s0
-  addi $s0, $zero, 1
-  # BINARY: 001000 10000 00000 0000000000000001
-  #         opcode rt    rs    immediate
+    addi $s0, $zero, 1
+  # addi  rt,    rs, immediate
+  # rt <- rs + immediate
+  # BINARY: 001000 00000 10000 0000000000000001
+  #         ADDI   0     16    1
+  #         opcode rs    rt    immediate
 
-  # put 2 in $s1
-  addi $s1, $zero, 2
-  # BINARY: 001000 10001 00000 0000000000000010
-  #         opcode rt    rs    immediate
+    addi $s1, $zero, 2
+  # addi  rt,    rs, immediate
+  # rt <- rs + immediate
+  # BINARY: 001000 00000 10001 0000000000000010
+  #         ADDI   0     17    2
+  #         opcode rs    rt    immediate
 
-  # put $s0 + $s1 (1 + 2 = 3) in $s2
-  add  $s2, $s0, $s1
-  # BINARY: 000000 10010 10000 10001 00000 100000
-  #         opcode rt    rs    rd    shamt funct
+    add  $s2, $s0, $s1
+  # add   rd,  rs,  rt
+  # rd <- rs + rt
+  # BINARY: 000000 10000 10001 10010 00000 100000
+  #         SPEC   16    17    18    0     ADD
+  #         opcode rs    rt    rd    shamt funct
 
-  # store $s2 (3) in memory location
-  sw, $s2, 0(0)
+    sw   $s2, 0(0)
+  # sw    rt, offset(base)
+  # memory[base + offset] <- rt
   # BINARY: 101011 00000 10010 0000000000000000
+  #         SW     0     18    0
   #         opcode base  rt    offset
